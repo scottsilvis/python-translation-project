bb4360fa7915ddf370d6ac9a406fc34ca1231a68
Scott Silvis <sds0047@vetmed.auburn.edu>
2023-03-03 12:18:36 -0600

    Finished
    I finally finished the part two of this assignment. The lecture on
    refular expressions was incredibly helpful. I was still confused on how
    to get the patter to accept variables, and my first attempt failed
    because it was expecting a string and got a list instead. some googling
    lead me to the .join function which made everything work.

--- find_orf.py
+++ find_orf.py
@@ -206,7 +206,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'(' + '|'.join(starts) + r')([ACGU]{3})*(' + '|'.join(stops) + r')'
     ##########################################################################
 
     # Create the regular expression object
